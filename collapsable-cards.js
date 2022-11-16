e582f21f288545d9362dede665f07b7226d00a59
Tomáš Holan <tomas@holan.dev>
2022-11-16 10:29:54 +0100

    Sync with upstream (#30)

--- collapsable-cards.js
+++ collapsable-cards.js
@@ -1,195 +1,231 @@
-console.log(`%ccollapsable-cards\n%cVersion: ${'0.0.1'}`, 'color: rebeccapurple; font-weight: bold;', '');
+class CollapsableCards extends HTMLElement {
 
-class VerticalStackInCard extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  setConfig(config) {
-    this.id = Math.round(Math.random() * 10000)
-    this._cardSize = {};
-    this._cardSize.promise = new Promise((resolve) => (this._cardSize.resolve = resolve));
-
-    if (!config || !config.cards || !Array.isArray(config.cards)) {
-      throw new Error('Supply the `cards` property');
-    }
-
-	let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-	if (config.defaultOpen == true) {
-	  this.isToggled = true;
-	} else if (config.defaultOpen == 'desktop-only' && !isMobile) {
-	  this.isToggled = true;
-	} else {
-	  this.isToggled = false;
+	constructor() {
+		super();
+		this.containers = ["grid", "custom:collapsable-cards", "vertical-stack", "horizontal-stack"];
+		this.differentConfig = ["HUI-THERMOSTAT-CARD"];
 	}
 
-    this._config = config;
-    this._refCards = [];
-    this._titleCard = null;
-    this.renderCard();
-  }
+	setConfig(config) {
+		this.id = Math.round(Math.random() * 10000)
+		this._cardSize = {};
+		this._cardSize.promise = new Promise((resolve) => (this._cardSize.resolve = resolve));
 
-  async renderCard() {
-    const config = this._config;
-    if (window.loadCardHelpers) {
-      this.helpers = await window.loadCardHelpers();
-    }
-    const promises = config.cards.map((config) => this.createCardElement(config));
-    this._refCards = await Promise.all(promises);
+		if (!config || !config.cards || !Array.isArray(config.cards)) {
+			throw new Error('Supply the `cards` property');
+		}
 
-    if (config.title_card) {
-      this._titleCard = await this.createCardElement(config.title_card);
-    }
+		let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+		if (config.defaultOpen == true) {
+			this.isToggled = true;
+		} else if (config.defaultOpen == 'desktop-only' && !isMobile) {
+			this.isToggled = true;
+		} else {
+			this.isToggled = false;
+		}
 
-    // Create the card
-    const card = document.createElement('ha-card');
-    this.card = card
-    const cardList = document.createElement('div');
-    this.cardList = cardList
-    card.style.overflow = 'hidden';
-    this._refCards.forEach((card) => cardList.appendChild(card));
-    this.cardList.className = 'card-list-' + this.id
-    this.cardList.classList[this.isToggled ? 'add' : 'remove']('is-toggled')
+		this._config = config;
+		this._refCards = [];
+		this._titleCard = null;
+		this.renderCard();
+	}
 
-    // create the button
-    const toggleButton = this.createToggleButton()
+	async renderCard() {
+		const config = this._config;
+		if (window.loadCardHelpers) {
+			this.helpers = await window.loadCardHelpers();
+		}
+		const promises = config.cards.map((config) => this.createCardElement(config));
+		this._refCards = await Promise.all(promises);
 
-    card.appendChild(toggleButton);
-    card.appendChild(cardList);
+		if (config.title_card) {
+			this._titleCard = await this.createCardElement(config.title_card);
+		}
 
-    while (this.hasChildNodes()) {
-      this.removeChild(this.lastChild);
-    }
-    this.appendChild(card);
+		// Create the card
+		const card = document.createElement('ha-card');
+		this.card = card
+		const cardList = document.createElement('div');
+		this.cardList = cardList
+		card.style.overflow = 'hidden';
+		this._refCards.forEach((card) => cardList.appendChild(card));
+		this.cardList.className = 'card-list-' + this.id
+		this.cardList.classList[this.isToggled ? 'add' : 'remove']('is-toggled')
 
-    // Calculate card size
-    this._cardSize.resolve();
+		// create the button
+		const toggleButton = this.createToggleButton()
 
-    const styleTag = document.createElement('style')
-    styleTag.innerHTML = this.getStyles()
-    card.appendChild(styleTag);
+		card.appendChild(toggleButton);
+		card.appendChild(cardList);
 
-    if (config.defaultOpen === 'contain-toggled' && config.cards.filter((c) => this.checkActiveCard(c)).length > 0) {
-      toggleButton.click();
-    }
-  }
-	
-  checkActiveCard(card) {
-	  const containers = ["grid", "vertical-stack", "horizontal-stack"];
-	  return containers.includes(card.type)
-		  ? card.cards.filter((c) => this.checkActiveCard(c)).length > 0
-	          : this._hass.states[card.entity]?.state !== "off"
-  }
+		while (this.hasChildNodes()) {
+			this.removeChild(this.lastChild);
+		}
+		this.appendChild(card);
 
-  createToggleButton() {
-    const toggleButton = document.createElement('button');
-    if (this._titleCard) {
-      toggleButton.append(this._titleCard);
-    } else {
-      toggleButton.innerHTML = this._config.title || 'Toggle';
-    }
-    toggleButton.className = 'card-content toggle-button-' + this.id
-    toggleButton.addEventListener('click', () => {
-      this.isToggled = !this.isToggled
-      this.styleCard(this.isToggled)
-    })
+		// Calculate card size
+		this._cardSize.resolve();
 
-    const icon = document.createElement('ha-icon');
-    icon.className = 'toggle-button__icon-' + this.id
-    icon.setAttribute('icon', 'mdi:chevron-down')
-    this.icon = icon
-    toggleButton.appendChild(icon)
+		const styleTag = document.createElement('style')
+		styleTag.innerHTML = this.getStyles()
+		card.appendChild(styleTag);
 
-    return toggleButton
-  }
+		if (config.defaultOpen === 'contain-toggled') {
+			this.toggle(this.isCardActive());
+		}
+	}
 
-  styleCard(isToggled) {
-    this.cardList.classList[isToggled ? 'add' : 'remove']('is-toggled')
-    this.icon.setAttribute('icon', isToggled ? 'mdi:chevron-up' : 'mdi:chevron-down')
-  }
+	getEntitiesNames(card) {
+		card = card.hasOwnProperty('tagName') && this.differentConfig.includes(card.tagName) ? card.___config : card;
 
-  async createCardElement(cardConfig) {
-    const createError = (error, origConfig) => {
-      return createThing('hui-error-card', {
-        type: 'error',
-        error,
-        origConfig
-      });
-    };
+		if (this.containers.includes(card.type))
+			return [].concat(... card.cards.map( (c) => this.getEntitiesNames(c) ));
 
-    const createThing = (tag, config) => {
-      if (this.helpers) {
-        if (config.type === 'divider') {
-          return this.helpers.createRowElement(config);
-        } else {
-          return this.helpers.createCardElement(config);
-        }
-      }
+		if (card.hasOwnProperty('entity'))
+			return [card.entity];
 
-      const element = document.createElement(tag);
-      try {
-        element.setConfig(config);
-      } catch (err) {
-        console.error(tag, err);
-        return createError(err.message, config);
-      }
-      return element;
-    };
+		if (card.hasOwnProperty('entities'))
+			return card.entities;
 
-    let tag = cardConfig.type;
-    if (tag.startsWith('divider')) {
-      tag = `hui-divider-row`;
-    } else if (tag.startsWith('custom:')) {
-      tag = tag.substr('custom:'.length);
-    } else {
-      tag = `hui-${tag}-card`;
-    }
+		return [];
+	}
 
-    const element = createThing(tag, cardConfig);
-    element.hass = this._hass;
-    element.addEventListener(
-      'll-rebuild',
-      (ev) => {
-        ev.stopPropagation();
-        this.createCardElement(cardConfig).then(() => {
-          this.renderCard();
-        });
-      },
-      { once: true }
-    );
-    return element;
-  }
+	isCardActive() {
+		return this.getEntitiesNames(this._config).filter((e) => this._hass.states[e].state !== "off").length > 0
+	}
 
-  set hass(hass) {
-    this._hass = hass;
-    if (this._refCards) {
-      this._refCards.forEach((card) => {
-        card.hass = hass;
-      });
-    }
-    if (this._titleCard) {
-      this._titleCard.hass = hass;
-    }
-  }
+	toggle(open = null) {
+		this.isToggled = open === null ? !this.isToggled : open;
+		this.styleCard(this.isToggled);
+	}
 
-  _computeCardSize(card) {
-    if (typeof card.getCardSize === 'function') {
-      return card.getCardSize();
-    }
-    return customElements
-      .whenDefined(card.localName)
-      .then(() => this._computeCardSize(card))
-      .catch(() => 1);
-  }
+	createToggleButton() {
+		const toggleButton = document.createElement('button');
+		if (this._titleCard) {
+			toggleButton.append(this._titleCard);
+		} else {
+			toggleButton.innerHTML = this._config.title || 'Toggle';
+		}
+		toggleButton.className = 'card-content toggle-button-' + this.id
+		toggleButton.addEventListener('click', () => {
+			this.isToggled = !this.isToggled
+			this.styleCard(this.isToggled)
+		})
 
-  async getCardSize() {
-    await this._cardSize.promise;
-    const sizes = await Promise.all(this._refCards.map(this._computeCardSize));
-    return sizes.reduce((a, b) => a + b);
-  }
+		const icon = document.createElement('ha-icon');
+		icon.className = 'toggle-button__icon-' + this.id
+		icon.setAttribute('icon', 'mdi:chevron-down')
+		this.icon = icon
+		toggleButton.appendChild(icon)
 
-  getStyles() {
-    return `
+		return toggleButton
+	}
+
+	styleCard(isToggled) {
+		this.cardList.classList[isToggled ? 'add' : 'remove']('is-toggled')
+		this.icon.setAttribute('icon', isToggled ? 'mdi:chevron-up' : 'mdi:chevron-down')
+	}
+
+	async createCardElement(cardConfig) {
+		const createError = (error, origConfig) => {
+			return createThing('hui-error-card', {
+				type: 'error',
+				error,
+				origConfig
+			});
+		};
+
+		const createThing = (tag, config) => {
+			if (this.helpers) {
+				if (config.type === 'divider') {
+					return this.helpers.createRowElement(config);
+				} else {
+					return this.helpers.createCardElement(config);
+				}
+			}
+
+			const element = document.createElement(tag);
+			try {
+				element.setConfig(config);
+			} catch (err) {
+				console.error(tag, err);
+				return createError(err.message, config);
+			}
+			return element;
+		};
+
+		let tag = cardConfig.type;
+		if (tag.startsWith('divider')) {
+			tag = `hui-divider-row`;
+		} else if (tag.startsWith('custom:')) {
+			tag = tag.substr('custom:'.length);
+		} else {
+			tag = `hui-${tag}-card`;
+		}
+
+		const element = createThing(tag, cardConfig);
+		element.hass = this._hass;
+		element.addEventListener(
+			'll-rebuild',
+			(ev) => {
+				ev.stopPropagation();
+				this.createCardElement(cardConfig).then(() => {
+					this.renderCard();
+				});
+			},
+			{ once: true }
+		);
+		return element;
+	}
+
+	redrawCard(oldHass) {
+		if (this._config.defaultOpen === 'contain-toggled' && this._haveEntitiesChanged(oldHass)) this.renderCard();
+	}
+
+	set hass(hass) {
+		let oldHass = this._hass;
+		this._hass = hass;
+
+		if (this._refCards) {
+			this._refCards.forEach((card) => {
+				card.hass = hass;
+			});
+		}
+
+		if (this._titleCard) {
+			this._titleCard.hass = hass;
+		}
+		this.redrawCard(oldHass);
+	}
+
+	_computeCardSize(card) {
+		if (typeof card.getCardSize === 'function') {
+			return card.getCardSize();
+		}
+		return customElements
+			.whenDefined(card.localName)
+			.then(() => this._computeCardSize(card))
+			.catch(() => 1);
+	}
+
+	_haveEntitiesChanged(oldHass) {
+		if (!this._hass || !oldHass) return true;
+
+		for (const entity of this.getEntitiesNames(this._config)) {
+			if (this._hass.states[entity].state !== oldHass.states[entity].state) return true;
+		}
+
+		return false;
+	}
+
+	async getCardSize() {
+		await this._cardSize.promise;
+		const sizes = await Promise.all(this._refCards.map(this._computeCardSize));
+		return sizes.reduce((a, b) => a + b);
+	}
+
+	getStyles() {
+		return `
       .toggle-button-${this.id} {
         color: var(--primary-text-color);
         text-align: left;
@@ -242,16 +278,16 @@ class VerticalStackInCard extends HTMLElement {
         background: transparent;
       }
     `;
-  }
+	}
 
 }
 
-customElements.define('collapsable-cards', VerticalStackInCard);
+customElements.define('collapsable-cards', CollapsableCards);
 
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: "collapsable-cards",
-  name: "Collapsable Card",
-  preview: false,
-  description: "The Collapsable Card allows you to hide other cards behind a dropdown toggle."
-});
+							type: "collapsable-cards",
+							name: "Collapsable Card",
+							preview: false,
+							description: "The Collapsable Card allows you to hide other cards behind a dropdown toggle."
+						});
